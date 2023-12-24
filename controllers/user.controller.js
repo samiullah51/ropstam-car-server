12d549991cce5770d916b8401708d05d111fe3d9
samiullah51 <samii51728@gmail.com>
2023-12-25 00:17:48 +0500

    JWT and login functionality added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ const User = require("../models/user.model");
 const generatePassword = require("../utils/generatePassword");
 const sendEmail = require("../utils/sendEmail");
 const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
 
 // @description       User registration
 // @route             POST http://localhost:8000/ropstam-car/api/v1/register
@@ -10,8 +11,8 @@ const register = async (req, res) => {
   try {
     const { firstName, lastName, email } = req.body;
     // check if user already exists or not
-    const checkUser = await User.find({ email });
-    if (checkUser.length > 0) {
+    const checkUser = await User.findOne({ email });
+    if (checkUser) {
       return res.status(409).json({
         success: false,
         data: { message: null, error: "email already exists" },
@@ -49,6 +50,46 @@ const register = async (req, res) => {
   }
 };
 
+// @description       User Login
+// @route             POST http://localhost:8000/ropstam-car/api/v1/login
+// @access            Public
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    // Check if user exist or not
+    if (!user) {
+      return res.status(401).json("user not found");
+    } else {
+      // Decrypt the password which is stored in Encryption form in database
+      const hashedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.CRYPTO_SEC
+      );
+      const realPassword = await hashedPassword.toString(CryptoJS.enc.Utf8);
+      if (realPassword !== req.body.password) {
+        return res.status(401).json(" Password is Incorrect");
+      } else {
+        // Create Token
+        const token = JWT.sign(
+          {
+            id: user._id,
+            isAdmin: user.isAdmin,
+          },
+          process.env.JWT_SEC,
+          { expiresIn: "3d" }
+        );
+        const { password, ...others } = user._doc;
+        res
+          .header("token", token)
+          .json({ success: true, data: { ...others, token }, error: null });
+      }
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, data: null, error: err.message });
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,10 @@
-const { register } = require("../controllers/user.controller");
+const { register, login } = require("../controllers/user.controller");
 
 const router = require("express").Router();
 
 // User Registration
 router.post("/register", register);
+// User Login
+router.post("/login", login);
 
 module.exports = router;
