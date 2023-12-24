39d782562c56e2f26217c7ede48094931b5470be
samiullah51 <samii51728@gmail.com>
2023-12-25 02:59:11 +0500

    image uploaded to s3

--- utils/s3Config.js
+++ utils/s3Config.js
@@ -2,8 +2,8 @@ const { S3Client } = require("@aws-sdk/client-s3");
 const s3 = new S3Client({
   region: "eu-north-1",
   credentials: {
-    accessKeyId: process.env.ACCESS_KEY,
-    secretAccessKey: process.env.SECRETE_ACCESS_KEY,
+    accessKeyId: "AKIA3UOW5B2DWJJSICAE",
+    secretAccessKey: "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",
   },
 });
 
